0dd972ec86369e84779316bcd6a228bd0b99b550
Deise Costa <deisecj@gmail.com>
2022-06-03 14:27:28 -0400

    code review changes

--- src/controllers/questionController.ts
+++ src/controllers/questionController.ts
@@ -22,21 +22,21 @@ export class QuestionController extends BaseController {
     create(req: Request, res: Response) {
         const { title, description } = req.body;
 
-        return checkRequest.validateQuestion(title, description)
-        .then((errors) => {
-            if (errors.length === 0) {
-                super.validateToken(req)
-                .then((auth) => {
+        return super.validateToken(req)
+        .then((auth) => { 
+            return checkRequest.validateQuestion(title, description)
+            .then((errors) => {
+                if (errors.length === 0) {
                     return  this.userRepository.getByEmail(auth.email)
                     .then((user) => {
                         const question = new Question({ title: title, description: description, user: user });
                         return this.questionRepository.save(question)
                         .then(() => res.json({ message: "question created with success" }));
                      });
-                }).catch((error) => super.handleErrors(res, error)); 
-            } else {
-                res.status(400).json(errors);
-            }
+                } else {
+                    res.status(400).json(errors);
+                }
+            });
         }).catch((error) => super.handleErrors(res, error));   
     }
 }
